f5945861af624b6cb74530ece0a3da24c0c4f406
Kalebe <kalebe130510@icloud.com>
2026-04-24 16:36:42 -0300

    init db fix

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from routes.tecnicos import tecnicos_bp
 app = Flask(__name__)
 CORS(app)
 
+init_db()
+
 app.register_blueprint(fichas_bp, url_prefix="/api")
 app.register_blueprint(servicos_bp, url_prefix="/api")
 app.register_blueprint(tecnicos_bp, url_prefix="/api")
@@ -18,6 +20,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
