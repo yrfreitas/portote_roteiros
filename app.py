c1722fb71301567619e0445aaadb92f16e36ccc0
Kalebe <kalebe130510@icloud.com>
2026-04-24 15:38:57 -0300

    técnicos, verificador de CEP e responsivo mobile

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from flask import Flask, render_template
 from database import init_db
 from routes.fichas import fichas_bp
 from routes.servicos import servicos_bp
+from routes.tecnicos import tecnicos_bp  # <-- IMPORTAÇÃO ADICIONADA
 
 app = Flask(__name__)
 
 app.register_blueprint(fichas_bp, url_prefix="/api")
 app.register_blueprint(servicos_bp, url_prefix="/api")
-
+app.register_blueprint(tecnicos_bp, url_prefix="/api") # <-- REGISTRO ADICIONADO
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
 
 if __name__ == "__main__":
     init_db()
